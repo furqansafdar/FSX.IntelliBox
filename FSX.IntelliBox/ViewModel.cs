7370b0f509bf065f51dd958a09731052a8af9f48
Furqan Safdar <furqan.safdar@gmail.com>
2015-11-18 18:37:29 +0300

    furqan.safdar | First release

--- FSX.IntelliBox/ViewModel.cs
+++ FSX.IntelliBox/ViewModel.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using FSX.Windows.Controls;
-using System.Collections.Generic;
 
 namespace FSX.IntelliBox
 {
